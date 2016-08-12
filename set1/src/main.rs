207eae972bdfda732ea9465720e33619b3951095
Brandon Matthews <bmatt@optimaltour.us>
2016-08-11 17:35:38 -0700

    Challenge 2

--- set1/src/main.rs
+++ set1/src/main.rs
@@ -1,5 +1,6 @@
 
 mod challenge1;
+mod challenge2;
 
 fn main() {
 
@@ -9,4 +10,14 @@ fn main() {
             "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"),
         "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
     println!("Challenge 1 okay");
+
+    // Challenge 2
+    assert_eq!(
+        challenge2::fixed_xor(
+            &challenge1::hex_decode("1c0111001f010100061a024b53535009181c"),
+            &challenge1::hex_decode("686974207468652062756c6c277320657965")),
+        challenge1::hex_decode("746865206b696420646f6e277420706c6179")
+        );
+    println!("Challenge 2 okay");
+
 }

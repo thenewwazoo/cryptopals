b8b4a3f1ed4a39be7075008ff90a4c4d91d953b0
Brandon Matthews <bmatt@optimaltour.us>
2016-08-15 17:31:21 -0700

    Challenge 5

--- set1/src/main.rs
+++ set1/src/main.rs
@@ -3,6 +3,7 @@ mod challenge1;
 mod challenge2;
 mod challenge3;
 mod challenge4;
+mod challenge5;
 
 fn main() {
 
@@ -31,4 +32,10 @@ fn main() {
     assert!(challenge4::do_find_ciphertext("4.txt", "Now that the party is jumping\n"));
     println!("Challenge 4 okay");
 
+    // Challenge 5
+    assert_eq!(
+        challenge5::rcx("ICE".as_bytes(), "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"),
+        "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+    );
+    println!("Challenge 5 okay");
 }

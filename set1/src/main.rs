87839bc6ee3b1d5a7d1e4ebebcca1d986876d1de
Brandon Matthews <bmatt@optimaltour.us>
2016-08-15 17:09:01 -0700

    Challenge 3

--- set1/src/main.rs
+++ set1/src/main.rs
@@ -1,6 +1,7 @@
 
 mod challenge1;
 mod challenge2;
+mod challenge3;
 
 fn main() {
 
@@ -16,8 +17,12 @@ fn main() {
         challenge2::fixed_xor(
             &challenge1::hex_decode("1c0111001f010100061a024b53535009181c"),
             &challenge1::hex_decode("686974207468652062756c6c277320657965")),
-        challenge1::hex_decode("746865206b696420646f6e277420706c6179")
-        );
+        challenge1::hex_decode("746865206b696420646f6e277420706c6179"));
     println!("Challenge 2 okay");
 
+    // Challenge 3
+    assert_eq!(
+        challenge3::decrypt_message("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"),
+        "Cooking MC's like a pound of bacon");
+    println!("Challenge 3 okay");
 }
